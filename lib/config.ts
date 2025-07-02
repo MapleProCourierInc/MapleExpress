761615db2ecf51b8d5ae36ceee948028a87471c8
Rishabh Khevaria <khevaria.rishabh@outlook.com>
2025-07-02 19:19:44 -0300

    Update UI of Email verification Page

--- lib/config.ts
+++ lib/config.ts
@@ -4,18 +4,18 @@
  */
 
 // Auth Service
-export const AUTH_MICROSERVICE_URL = process.env.AUTH_MICROSERVICE_URL || 'http://localhost:30080/usermanagement/auth';
+export const AUTH_MICROSERVICE_URL = process.env.AUTH_MICROSERVICE_URL || 'https://testapi.maplexpress.ca/usermanagement/auth';
 export const AUTH_REFRESH_URL = process.env.AUTH_REFRESH_URL || `${AUTH_MICROSERVICE_URL}/refresh`;
 export const AUTH_API_KEY = process.env.AUTH_API_KEY || '';
 
 // Profile Service
-export const PROFILE_SERVICE_URL = process.env.NEXT_PUBLIC_PROFILE_SERVICE_URL || 'http://localhost:30081/usermanagement';
+export const PROFILE_SERVICE_URL = process.env.NEXT_PUBLIC_PROFILE_SERVICE_URL || 'https://testapi.maplexpress.ca/usermanagement';
 
 // Order Service
-export const ORDER_SERVICE_URL = process.env.NEXT_PUBLIC_ORDER_SERVICE_URL || 'http://localhost:30082/ordermanagement';
+export const ORDER_SERVICE_URL = process.env.NEXT_PUBLIC_ORDER_SERVICE_URL || 'https://testapi.maplexpress.ca/ordermanagement';
 
 // Payment Service
-export const PRICING_PAYMENT_SERVICE_URL = process.env.NEXT_PUBLIC_PRICING_PAYMENT_SERVICE_URL || 'http://localhost:30083/paymentservice';
+export const PRICING_PAYMENT_SERVICE_URL = process.env.NEXT_PUBLIC_PRICING_PAYMENT_SERVICE_URL || 'https://testapi.maplexpress.ca/paymentservice';
 
 // Google Maps API
 export const GOOGLE_MAPS_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || 'AIzaSyA3wSq5PrN2evVDM5MiRKPa4ZS69taWS8E';
